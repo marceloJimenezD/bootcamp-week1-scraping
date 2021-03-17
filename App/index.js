2b94516fcaa02d13f47a8e001490e4af5911891c
marceloJimenezD <64915545+marceloJimenezD@users.noreply.github.com>
2021-03-17 11:31:44 -0500

    terminado el reto .... espero

--- App/index.js
+++ App/index.js
@@ -1,5 +1,7 @@
 let btnscrap = document.getElementById('scrap-profile')
 
+
+
 btnscrap.addEventListener('click', async ()=>{
     const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
@@ -20,7 +22,7 @@ const scrapingProfile = ()=>{
         });
     };
 
-    const atuoScroll = async ()=>{
+    const atuoScrollScanner = async ()=>{
         let exist = document.querySelector('body');
         let oldY = -1;
         while (exist) {
@@ -33,7 +35,10 @@ const scrapingProfile = ()=>{
             oldY = actualY;
         }
         
-        
+
+        /////////////////obteniendo Experiencia
+        const btnMoreExperience = document.querySelectorAll("#experience-section button");
+        Array.from(btnMoreExperience).map((btn)=>{ btn.click()})
 
         const elementExperience = document.querySelectorAll("#experience-section ul.pv-profile-section__section-info > li section");
         const experience = elementExperience? Array.from(elementExperience).map((section)=>{
@@ -42,11 +47,16 @@ const scrapingProfile = ()=>{
                 return {cargo: li.querySelectorAll('h3')[0].children[1].innerText,periodo:li.querySelectorAll('div h4')[0].innerText};
             })]} 
             : {empresa:section.querySelectorAll('div.pv-entity__summary-info p')[1].innerText,cargos:[{cargo:section.querySelectorAll('div.pv-entity__summary-info h3')[0].innerText,periodo:section.querySelectorAll('div.pv-entity__summary-info h4')[0].children[1].innerText}]};
-        }):'';
-
-        
+        }):[];
+           
         
         data.experiencia=experience;     
+
+
+
+        /////////////Oteniendo Educacion
+        const btnMoreEducation = document.querySelectorAll("#education-section button");
+        Array.from(btnMoreEducation).map((btn)=>{ btn.click()})
 
 
         const elementEducation = document.querySelectorAll("section.education-section  ul li.pv-profile-section__list-item div.pv-entity__summary-info");
@@ -57,20 +67,23 @@ const scrapingProfile = ()=>{
                 especialidad: li.children[0].children[2]? li.children[0].children[2].children[1].innerText:'',
                 periodo:li.children[1]? li.children[1].children[1].innerText:'',
             };
-        }):'';
+        }):[];
         
         data.educacion= education;
 
+
+        ////////////////////////////////////
+        console.log(data);
+
     };
     
-
+    ////// El algoritmo en si
     const elementNameProfile = document.querySelector("div.ph5.pb5 > div.display-flex.mt2 ul li")
     const elementNameTitle = document.querySelector("div.ph5.pb5 > div.display-flex.mt2 h2")
     const elemntUbicacion = document.querySelector("div.ph5.pb5 > div.display-flex.mt2 ul.pv-top-card--list.pv-top-card--list-bullet.mt1 li.t-16.t-black.t-normal.inline-block");//por ahora
     const name = elementNameProfile? elementNameProfile.innerText:'';
     const title = elementNameTitle? elementNameTitle.innerText:'';
     const ubicacion = elemntUbicacion? elemntUbicacion.innerText:'';
-    
     wait(2000)
     const elementMoreResume = document.getElementById('line-clamp-show-more-button')
     if(elementMoreResume) elementMoreResume.click();
@@ -85,11 +98,10 @@ const scrapingProfile = ()=>{
         experiencia:undefined,
         educacion:undefined
     };
-
-
+   
+    atuoScrollScanner();
     
-    atuoScroll();
-    console.log(data);
 }
+
 
 
